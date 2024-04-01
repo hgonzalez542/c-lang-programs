fd2bed4b0a75c3645ec381aae822f9c5924a102c
Hector Gonzalez <hgonzalez542@g.rwu.edu>
2024-04-01 11:19:56 -0400

    three programs executred

---
+++ c-to-asm.c
@@ -0,0 +1,17 @@
+// C code stored in c-to-asm.c file
+// Hector Gonzalez, 03/29/24, Assignment 7, ** C-to-asm.c **
+
+#include <stdio.h>
+ 
+// global string
+char s[] = "Hello RWU!";
+ 
+// Driver Code
+int main()
+{
+    // Declaring variables
+    int a = 2000, b =24;
+     
+    // Printing statement
+    printf("%s %d \n", s, a+b);
+}
---
+++ caesars-cipher.c
@@ -0,0 +1,29 @@
+// Hector Gonzalez, 03/29/24, Assignment 7, ** Caesars-cipher.c **
+
+#include<iostream>
+#include<string>
+
+/* 
+
+E_n(x)=(x+n)mod\ 26           
+(Encryption Phase with shift n)
+
+D_n(x)=(x-n)mod\ 26           
+(Decryption Phase with shift n)
+*/
+
+using namespace std;
+int main(){
+    int i,j,k;
+    string s,t;
+    int key;
+    cout<<"\nEnter the key: ";
+    cin>>key;
+    cout<<"\nEnter the message: ";
+    cin>>s;
+    for(i=0;i<s.size();i++){
+        t+=(s[i]-'A'+key)%26+'A';
+    }
+    cout<<"\n\nEncrypted message: "<<t<<'\n';
+    return 0;
+}
